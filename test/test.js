a585375aa1383e4b30da63a5103eceaca7f7703d
deathcap <deathcap@gmx.co.uk>
2015-03-22 19:45:27 -0700

    Add testling

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var test = require('tape');
-var hex = require('./');
+var hex = require('../');
 
 test('empty buffer', function(t) {
   t.equal(hex(new Buffer([])), 'Offset   00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F');
